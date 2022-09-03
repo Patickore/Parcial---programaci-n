ed19b5fc7e77c5661e35a0ef8f49741aa3337a0d
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-09-02 19:01:32 -0600

    Agregar archivos de proyecto.

---
+++ Parcial - programación/Estudiantes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Parcial___programación
+{
+    internal class Estudiantes
+    {
+        internal int NotaFinal;
+
+        public Estudiantes(int id, string codigo, string nombre, int edad, int nota1, int nota2, int notaFinal)
+        {
+            Codigo = codigo;
+            NotaFinal = notaFinal;
+        }
+
+        public Estudiantes(int id, string codigo, string nombre, int edad, string facultad, string asignatura, int nota1, int nota2, int notaFinal)
+        {
+            Codigo = codigo;
+            NotaFinal = notaFinal;
+        }
+
+        public string Codigo { get; internal set; }
+
+        internal bool mostrar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
